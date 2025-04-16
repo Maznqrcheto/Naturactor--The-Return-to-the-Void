c72f3b49cf2595a830dcdef70d3ac3db121a0717
Aleksandar Stanchev <altryx15@gmail.com>
2025-04-16 22:03:02 +0300

    Fixed Camera

--- Scripts/CameraMovement.cs
+++ Scripts/CameraMovement.cs
@@ -6,7 +6,7 @@ public class CameraMovement : MonoBehaviour
     public GenerateMap mapGen;
     void Update()
     {
-        Vector3 toMove = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        Vector3 toMove = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
         if ((transform.position.x + toMove.x) < 35
             || (transform.position.x + toMove.x) > mapGen.GetComponent<GenerateMap>().x - 35)
             toMove.x = 0;
