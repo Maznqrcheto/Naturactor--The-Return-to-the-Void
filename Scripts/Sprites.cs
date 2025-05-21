ecf8da147fa325af8c12e12d3a461cd8a0d94d52
Aleksandar Stanchev <altryx15@gmail.com>
2025-05-21 10:26:31 +0300

    Create Sprites.cs

---
+++ Scripts/Sprites.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Sprites : MonoBehaviour
+{
+    public List<Sprite> GrassSprites = new List<Sprite>();
+    public List<Sprite> GrassCliff = new List<Sprite>();
+    public List<Sprite> WaterSprites = new List<Sprite>();
+    public List<Sprite> CoalSprites = new List<Sprite>();
+    public List<Sprite> IronSprites = new List<Sprite>();
+    public List<Sprite> CopperSprites = new List<Sprite>();
+    public List<Sprite> TreeSprites = new List<Sprite>();
+    public List<Sprite> NaturactorSprites = new List<Sprite>();
+    public List<Sprite> VolcanoSprites = new List<Sprite>();
+
+    public List<Sprite> DrillSprites = new List<Sprite>();
+    public List<Sprite> RefinerySprites = new List<Sprite>();
+    public List<Sprite> ConveyorSprites = new List<Sprite>();
+    public List<Sprite> GeneratorSprites = new List<Sprite>();
+    public List<Sprite> CrafterSprites = new List<Sprite>();
+    public List<Sprite> StorageContainerSprites = new List<Sprite>();
+    public List<Sprite> LumberCampSprites = new List<Sprite>();
+    public List<Sprite> SmelterSprites = new List<Sprite>();
+
+    public List<Sprite> FireTreeSprites = new List<Sprite>();
+    public List<Sprite> FireGrassSprites = new List<Sprite>();
+    public List<Sprite> FireGrassCliffSprites = new List<Sprite>();
+    
+    
+    // public List<Sprite> ItemSprites;  -> mn e zle trqq sa pomisli
+
+
+}
