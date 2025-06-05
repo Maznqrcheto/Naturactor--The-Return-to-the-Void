d6e727fbbdd10adb0469908438ac4d6a5fb9b629
Aleksandar Stanchev <altryx15@gmail.com>
2025-06-05 19:39:20 +0300

    Update AchievementManager.cs

--- Scripts/Achievement Scripts/AchievementManager.cs
+++ Scripts/Achievement Scripts/AchievementManager.cs
@@ -6,19 +6,48 @@ public class AchievementManager : MonoBehaviour
 
     public List<Achievement> Achievements = new List<Achievement>();
 
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     public void UnlockAchievement(string title)
     {
-        Achievement achievement = Achievements.Find(a => a.Title == title);
+        Achievement achievement = null;
+        for (int i = 0; i < Achievements.Count; i++)
+        {
+            if (Achievements[i].Title == title)
+                {
+                    achievement = Achievements[i];
+                    break;
+                }
+        }
+
         if (achievement != null && !achievement.IsUnlocked)
         {
             achievement.IsUnlocked = true;
-            Debug.Log($"Achievement Unlocked: {achievement.Title}");
+            Debug.Log("Achievement Unlocked: " + achievement.Title);
         }
     }
 
     public bool IsAchievementUnlocked(string title)
     {
-        Achievement achievement = Achievements.Find(a => a.Title == title);
-        return achievement != null && achievement.IsUnlocked;
+        for (int i = 0; i < Achievements.Count; i++)
+        {
+            if (Achievements[i].Title == title)
+            {
+                return Achievements[i].IsUnlocked;
+            }
+        }
+
+        return false;
     }
 }
