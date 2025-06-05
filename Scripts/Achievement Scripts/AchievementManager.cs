6f2bbc90db71f9d2aa8a34fa22437347074a7ba2
Aleksandar Stanchev <altryx15@gmail.com>
2025-06-05 19:17:48 +0300

    Update AchievementManager.cs

--- Scripts/Achievement Scripts/AchievementManager.cs
+++ Scripts/Achievement Scripts/AchievementManager.cs
@@ -2,5 +2,23 @@ using UnityEngine;
 using System.Collections.Generic;
 public class AchievementManager : MonoBehaviour
 {
-    
+    public static AchievementManager Instance;
+
+    public List<Achievement> Achievements = new List<Achievement>();
+
+    public void UnlockAchievement(string title)
+    {
+        Achievement achievement = Achievements.Find(a => a.Title == title);
+        if (achievement != null && !achievement.IsUnlocked)
+        {
+            achievement.IsUnlocked = true;
+            Debug.Log($"Achievement Unlocked: {achievement.Title}");
+        }
+    }
+
+    public bool IsAchievementUnlocked(string title)
+    {
+        Achievement achievement = Achievements.Find(a => a.Title == title);
+        return achievement != null && achievement.IsUnlocked;
+    }
 }
